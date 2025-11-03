7d92dcc94e31f4bd6cb10411059c92a2831d818d
kati perttula <kati.perttula@centria.fi>
2025-11-03 18:14:51 +0200

    testii

--- viikko2/lue_varaukset.py
+++ viikko2/lue_varaukset.py
@@ -2,6 +2,6 @@ def main():
     varaukset = "varaukset.txt"
 
     with open(varaukset, "r", encoding="utf-8") as f:
-        varaus f.read().strip()
+        varaus = f.read().strip()
 
         print(varaus)
