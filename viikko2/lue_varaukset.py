a7769309875560d427b13930552a2e57bb32291a
kati perttula <kati.perttula@centria.fi>
2025-11-11 13:20:45 +0200

    juu

--- viikko2/lue_varaukset.py
+++ viikko2/lue_varaukset.py
@@ -1,35 +1,27 @@
+from datetime import datetime
 def main():
     varaukset = "varaukset.txt"
-
 
     with open("varaukset.txt", "r", encoding="utf-8") as f:
         for rivi in f:
             varaus = rivi.strip().split("|")
-           
-
-
-    from datetime import datetime
+              
     paiva = datetime.strptime(varaus[2], "%Y-%m-%d").date()
     suomalainenPaiva = paiva.strftime("%d.%m.%Y")
     aika = datetime.strptime(varaus[3], "%H:%M").time()
     suomalainenAika = aika.strftime("%H.%M")
-
     
     Varausnumero = int(varaus[0])
-    Varaaja = str(varaus[1])
-    #Varauspäivä = datetime.date(varaus[2])
-    #aloitusaika = datetime.time(varaus[3])
+    Varaaja = varaus[1]
     Tuntimäärä = float(varaus[4])
     Tuntihinta = float(varaus[5])
     Tuntihinta_str = f"{Tuntihinta:.2f}".replace(".", ",")
     Kokonaishinta = Tuntimäärä * Tuntihinta
     Kokonaishinta_str = f"{Kokonaishinta:.2f}".replace(".", ",")
-    Maksettu = bool(varaus[6])
-    Varauskohde = str(varaus[7])
-    Puhelinnumero = str(varaus[8])
-    Sähköposti = str (varaus[9])
-
-   
+    Maksettu = varaus[6].lower() == "true"
+    Varauskohde = varaus[7]
+    Puhelinnumero = varaus[8]
+    Sähköposti = varaus[9]
 
     print(f"Varausnumero:{Varausnumero}")
     print(f"Varaaja:{Varaaja}")
@@ -38,15 +30,11 @@ def main():
     print(f"Päivämäärä:{suomalainenPaiva}")
     print(f"Aloitusaika:{suomalainenAika}")
     print(f"Kokonaishinta:{Kokonaishinta_str} $")
-        #print(f"Maksettu:{Maksettu}")
     print(f"Maksettu: {'Kyllä' if Maksettu else 'Ei'}")
     print(f"Varauskohde:{Varauskohde}")
     print(f"Puhelinnumero:{Puhelinnumero}")
     print(f"Sähköposti:{Sähköposti}")
     print("-" * 40)  # erotin rivien väliin
-   
- 
-  
 
 if __name__ == "__main__":
     main()
