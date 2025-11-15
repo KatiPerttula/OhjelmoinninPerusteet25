d1bac89f7b15e40c5e712f8350f2052d7ca7cdc7
kati perttula <kati.perttula@centria.fi>
2025-11-15 21:10:48 +0200

    juu

--- viikko3/lue_varaukset.py
+++ viikko3/lue_varaukset.py
@@ -1,4 +1,4 @@
-from datetime import datetime #timedelta
+from datetime import datetime, timedelta
 
 def hae_varausnumero(varaus):
     return int(varaus[0])
@@ -27,42 +27,46 @@ def hae_puhelin(varaus):
 def hae_sahkoposti(varaus):
     return str(varaus[9])
 
+def tulosta_varaus(varaus):
+    Varausnumero = hae_varausnumero(varaus)
+    Varaaja = hae_varaaja(varaus)
+
+    paiva = hae_paiva(varaus)
+    suomalainenPaiva = paiva.strftime("%d.%m.%Y")
+    aika = datetime.strptime(varaus[3], "%H:%M").time()
+    suomalainenAika = aika.strftime("%H.%M")
+
+    Tuntimäärä = hae_Tuntimaara(varaus)
+    Tuntihinta = hae_Tuntihinta(varaus)
+    Tuntihinta_str = f"{Tuntihinta:.2f}".replace(".", ",")
+    Kokonaishinta = laske_kokonaishinta(varaus)
+    Kokonaishinta_str = f"{Kokonaishinta:.2f}".replace(".", ",")
+    Maksettu = hae_maksettu(varaus)
+    Varauskohde = hae_kohde(varaus)
+    Puhelinnumero = hae_puhelin(varaus)
+    Sähköposti = hae_sahkoposti(varaus)          
+              
+    print(f"Varausnumero: {Varausnumero}")
+    print(f"Varaaja: {Varaaja}")
+    print(f"Päivämäärä: {suomalainenPaiva}")
+    print(f"Aloitusaika: {suomalainenAika}")
+    print(f"Tuntimäärä: {Tuntimäärä}")
+    print(f"Tuntihinta: {Tuntihinta_str} €")
+    print(f"Kokonaishinta: {Kokonaishinta_str} €")
+    print(f"Maksettu: {'Kyllä' if Maksettu else 'Ei'}")
+    print(f"Varauskohde: {Varauskohde}")
+    print(f"Puhelinnumero: {Puhelinnumero}")
+    print(f"Sähköposti: {Sähköposti}")
+    
+
 def main():
     varaukset = "varaukset.txt"
         
     with open(varaukset, "r", encoding="utf-8") as f:
         for rivi in f:
             varaus = rivi.strip().split("|")
+            tulosta_varaus(varaus)
             
-            Varausnumero = hae_varausnumero(varaus)
-            Varaaja = hae_varaaja(varaus)
-
-            paiva = hae_paiva(varaus)
-            suomalainenPaiva = paiva.strftime("%d.%m.%Y")
-            aika = datetime.strptime(varaus[3], "%H:%M").time()
-            suomalainenAika = aika.strftime("%H.%M")
-
-            Tuntimäärä = hae_Tuntimaara(varaus)
-            Tuntihinta = hae_Tuntihinta(varaus)
-            Tuntihinta_str = f"{Tuntihinta:.2f}".replace(".", ",")
-            Kokonaishinta = laske_kokonaishinta(varaus)
-            Kokonaishinta_str = f"{Kokonaishinta:.2f}".replace(".", ",")
-            Maksettu = hae_maksettu(varaus)
-            Varauskohde = hae_kohde(varaus)
-            Puhelinnumero = hae_puhelin(varaus)
-            Sähköposti = hae_sahkoposti(varaus)          
-              
-            print(f"Varausnumero: {Varausnumero}")
-            print(f"Varaaja: {Varaaja}")
-            print(f"Päivämäärä: {suomalainenPaiva}")
-            print(f"Aloitusaika: {suomalainenAika}")
-            print(f"Tuntimäärä: {Tuntimäärä}")
-            print(f"Tuntihinta: {Tuntihinta_str} €")
-            print(f"Kokonaishinta: {Kokonaishinta_str} €")
-            print(f"Maksettu: {'Kyllä' if Maksettu else 'Ei'}")
-            print(f"Varauskohde: {Varauskohde}")
-            print(f"Puhelinnumero: {Puhelinnumero}")
-            print(f"Sähköposti: {Sähköposti}")
             
             #Nope
 if __name__ == "__main__":
