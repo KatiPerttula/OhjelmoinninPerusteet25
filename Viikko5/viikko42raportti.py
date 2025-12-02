ee546e4e1c45718a884ba6fa5ff69b02d55442ca
kati perttula <kati.perttula@centria.fi>
2025-12-03 01:42:38 +0200

    jipii

--- Viikko5/viikko42raportti.py
+++ Viikko5/viikko42raportti.py
@@ -1,7 +1,20 @@
+
 def lue_data(tiedoston_nimi: str) -> list:
-    print(tiedoston_nimi)
+    
+    kulutusTuotantoTiedot = []
+    with open(tiedoston_nimi, "r", encoding="utf-8") as f:
+        for kulutusTuotantoTieto in f:
+            varaus = kulutusTuotantoTieto.strip()
+            kulutusTuotantoTieto = kulutusTuotantoTieto.strip()
+            kulutusTuotantoTietoSarakkeet = kulutusTuotantoTieto.split(';')
+            kulutusTuotantoTiedot.append(kulutusTuotantoTietoSarakkeet)
+    return kulutusTuotantoTiedot
+    
 
 def main():
     print(lue_data("viikko42.csv"))
 
-        
+
+
+if __name__ == "__main__":
+    main()
